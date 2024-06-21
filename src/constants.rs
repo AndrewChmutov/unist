cee24da1137b8a3d23490004c1e5099fbb437ac9
Andrew Chmutov <ulman.andre@gmail.com>
2024-06-21 23:08:46 +0200

    Change name of state dir

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub static DAYS_LEFT: i32 = 2;
-pub static TABLE_PATH: &str = ".local/state/rasker/";
+pub static TABLE_PATH: &str = ".local/state/unist/";
 pub static TABLE_NAME: &str = "kek.csv";
 pub static SEPARATOR: &str =        "------------------------------";
 pub static FLUSH_ERROR: &str = "Could not flush to the standard output";
